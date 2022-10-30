1f35e3f1b323758d6bd2aec59dbece4fec49ed92
Sachin Sharma <sachin21559@iiitd.ac.in>
2022-10-31 00:45:18 +0530

    v3 (Updated ls to take dir)

--- src/ls.c
+++ src/ls.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <time.h>
 #include <sys/stat.h>
 
-void print_stats(char *fileName, struct stat stats);
+void print_stats(char *dirPath, char *fileName, struct stat stats);
 
 int main(int argc, char **argv)
 {
     int aFlag = 0;
     int lFlag = 0;
+    char *dirPath = ".";
     struct dirent *dirent;
     struct stat stats;
 
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-a") == 0)
+        {
             aFlag = 1;
+            continue;
+        }
         if (strcmp(argv[i], "-l") == 0)
+        {
             lFlag = 1;
+            continue;
+        }
+        dirPath = argv[i];
     }
 
-    DIR *dir = opendir(".");
+    DIR *dir = opendir(dirPath);
 
     if (dir == NULL)
     {
@@ -35,9 +44,8 @@ int main(int argc, char **argv)
             continue;
 
         if (lFlag == 1)
-        {
-            print_stats(dirent->d_name, stats);
-        }
+            print_stats(dirPath, dirent->d_name, stats);
+
         else
             printf("%s  ", dirent->d_name);
     }
@@ -48,18 +56,19 @@ int main(int argc, char **argv)
     {
         while ((dirent = readdir(dir)) != NULL)
         {
-            if (dirent->d_name[0] == '.' && strlen(dirent->d_name) > 2)
+            if (dirent->d_name[0] == '.' && strcmp(dirent->d_name, "..") != 0 && strlen(dirent->d_name) > 1) // for archived folders
             {
                 if (lFlag == 1)
-                    print_stats(dirent->d_name, stats);
+                    print_stats(dirPath, dirent->d_name, stats);
+
                 else
                     printf("%s  ", dirent->d_name);
             }
         }
         if (lFlag == 1)
         {
-            print_stats(".", stats);
-            print_stats("..", stats);
+            print_stats(dirPath, ".", stats);
+            print_stats(dirPath, "..", stats);
         }
         else
             printf(".  ..");
@@ -70,15 +79,20 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void print_stats(char *fileName, struct stat stats)
+void print_stats(char *dirPath, char *fileName, struct stat stats)
 {
     char buffer[40];
+    char *filePath = (char *)malloc(1000);
+    strcpy(filePath, dirPath);
+    strcat(filePath, "/");
+    strcat(filePath, fileName);
 
-    stat((const char *)fileName, &stats);
+    stat((const char *)filePath, &stats);
 
     strftime(buffer, 40, "%d %B %Y %H:%M:%S IST", localtime((const time_t *)&stats.st_ctim));
 
     printf("%s ", buffer);
     printf("%6d ", (int)stats.st_size);
     printf("%s\n", fileName);
+    free(filePath);
 }
